c807c8bdb2cd0ea8195b08eb06f37952a52f0599
Sharain Islam Anoy <91452150+anoy17@users.noreply.github.com>
2021-10-18 15:06:15 +0600

    Update Page 3 [Transaction History].php

--- Page 3 [Transaction History].php
+++ Page 3 [Transaction History].php
@@ -15,9 +15,9 @@
 
 <legend><b>Digital Wallet</b></legend>
 <br>
-<a href = Page 1 [Home].php> 1.Home</a>      
-<a href = Page 2 [Add Beneficiary].php> 2.Add Beneficiary</a>
-<a href = Page 3 [Transaction Hitory].php> 3.Transaction Hitory</a>
+<a href = http://localhost/Digital%20wallet/Page%201%20%5bHome%5d.php> 1.Home</a>      
+<a href = http://localhost/Digital%20wallet/Page%202%20%5bAdd%20Beneficiary%5d.php> 2.Add Beneficiary</a>
+<a href = http://localhost/Digital%20wallet/Page%203%20%5bTransaction%20History%5d.php> 3.Transaction Hitory</a>
 <br>
 <br>
 <label for="from">From:</label>
